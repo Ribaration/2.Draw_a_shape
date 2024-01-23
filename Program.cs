b6ec1767517157d14b1ccff3b00ea0c8cfe31841
Dell-User <Dell-User@DESKTOP-221INCO>
2024-01-23 09:26:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Draw_a_shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      /|");
+            Console.WriteLine("     / |");
+            Console.WriteLine("    /  |");
+            Console.WriteLine("   /   |");
+            Console.WriteLine(" _------");
+            //to pause the console otherwise it will close on its own
+            Console.ReadLine();
+        }
+    }
+}
